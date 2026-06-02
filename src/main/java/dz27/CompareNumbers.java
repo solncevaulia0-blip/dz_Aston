21c70421d2986de1cc0edda732776de96c89ed3a
Solntseva Julia <solncevaulia0@gmail.com>
2026-06-02 23:54:56 +0300

    dz.2.7

--- src/main/java/dz27/CompareNumbers.java
+++ src/main/java/dz27/CompareNumbers.java
@@ -3,22 +3,23 @@ package dz27;
 import java.util.Scanner;
 
 public class CompareNumbers {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Первое число:");
-        int a = sc.nextInt();
-
-        System.out.println("Второе число:");
-        int b = sc.nextInt();
+    public static String compare(int a, int b) {
 
         if (a > b) {
-            System.out.println("Первое больше");
+            return "Первое больше";
         } else if (a < b) {
-            System.out.println("Второе больше");
-        } else {
-            System.out.println("Числа равны");
+            return "Второе больше";
         }
+        return "Числа равны";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Первое число:");
+        int a = sc.nextInt();
+        System.out.println("Второе число:");
+        int b = sc.nextInt();
+        System.out.println(compare(a, b));
     }
 }
--- src/main/java/dz27/Factorial.java
+++ src/main/java/dz27/Factorial.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Factorial {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Введите число:");
-
-        int n = sc.nextInt();
+    public static int calculateFactorial(int n) {
         int factorial = 1;
         for (int i = 1; i <= n; i++) {
-            factorial = factorial * i;
+            factorial *= i;
         }
+        return factorial;
+    }
 
-        System.out.println("Факториал = " + factorial);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int n = sc.nextInt();
+        System.out.println("Факториал = " + calculateFactorial(n));
     }
 }
